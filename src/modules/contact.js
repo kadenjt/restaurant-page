c4655200ae378a896dcb3d30fe41706542c2eada
Kaden Thielmann <kadenthielmann@gmail.com>
2020-11-25 12:57:15 -0500

    Add restaurant address

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -3,17 +3,30 @@ const contactUs = () => {
         const container = document.querySelector("#content");
         const contact = document.createElement("div");
         contact.className = "contactUs";
-        //display contact
-        const address = document.createElement("h4");
-        address.className = "contact";
-        address.textContent = "Address"
         //Create map
         const frame = document.createElement('iframe');
         frame.setAttribute('id', 'map');
         frame.setAttribute('src', 'https://www.openstreetmap.org/export/embed.html?bbox=139.70230937004092%2C35.69233240514018%2C139.70555484294894%2C35.695133761876924&amp;layer=mapnik');
-        //append elements to the page
+        //display contact info
+        const info = document.createElement("div");
+        info.className = "contactInfo";
+        const address = document.createElement("h3");
+        address.textContent = "Address";
+        //const linebreak = document.createElement("br");
+        //address.appendChild(linebreak);
+        const line1Address = document.createElement("p");
+        line1Address.textContent = "2172 Street Rd."
+        //const linebreak2 = document.createElement("br");
+        //address.appendChild(linebreak2);
+        const line2Address = document.createElement("p");
+        line2Address.textContent = "Tokyo, Japan"
+        info.appendChild(address);
+        info.appendChild(line1Address);
+        info.appendChild(line2Address);
+        //assemble contact info
         contact.appendChild(frame);
-        contact.appendChild(address);
+        contact.appendChild(info);
+        //append contact sections to the page
         container.appendChild(contact);
     }
     return {displayItems}
