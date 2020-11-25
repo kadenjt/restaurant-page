39389b7ff186081ccf7ad89073720105ab02c745
Kaden Thielmann <kadenthielmann@gmail.com>
2020-11-25 12:56:40 -0500

    Add images to home page

--- src/modules/home.js
+++ src/modules/home.js
@@ -8,8 +8,30 @@ const home = () => {
         name.style.backgroundColor = "red";
         name.id = "displayName";
         name.textContent = "Samurai Sushi"
+        //
+        const imageDisplay = document.createElement("div");
+        imageDisplay.className = "homeImageDisplay";
+        //
+        const beefTeriyakiImage = document.createElement("img");
+        beefTeriyakiImage.className = "homeImage";
+        beefTeriyakiImage.src = "./img/beef_teriyaki.jpg";
+        beefTeriyakiImage.alt = "Beef Teriyaki."
+        imageDisplay.appendChild(beefTeriyakiImage);
+        //
+        const salmonTeriyakiImage = document.createElement("img");
+        salmonTeriyakiImage.className = "homeImage";
+        salmonTeriyakiImage.src = "./img/salmon_teriyaki.jpg";
+        salmonTeriyakiImage.alt = "Salmon Teriyaki."
+        imageDisplay.appendChild(salmonTeriyakiImage);
+        //
+        const sashimiBoatImage = document.createElement("img");
+        sashimiBoatImage.className = "homeImage";
+        sashimiBoatImage.src = "./img/sashimi_boat.jpg";
+        sashimiBoatImage.alt = "Sashimi Boat."
+        imageDisplay.appendChild(sashimiBoatImage);
         //append elements to the page
         welcome.appendChild(name);
+        welcome.appendChild(imageDisplay);
         container.appendChild(welcome);
     }
     return {displayTop}
